c1eafd90746a6bbca6c62e2fa46a716f51b2ef48
Shahen Algoo <shahenalgoo@gmail.com>
2025-01-01 17:34:59 +0400

    in-game pause menu

--- components/in-game-pause/index.tsx
+++ components/in-game-pause/index.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useApplicationStore } from "@/store/use-application-store";
-import { FC, useCallback, useEffect } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { ReactUnityEventParameter } from "react-unity-webgl/distribution/types/react-unity-event-parameters";
 import Dialog from "../ui/dialog";
 import { Button } from "../ui/button";
+import { Switch } from "../ui/switch";
+import { Label } from "../ui/label";
 
 interface InGamePauseProps {
 	addEventListener: (eventName: string, callback: (...parameters: ReactUnityEventParameter[]) => ReactUnityEventParameter) => void;
@@ -12,19 +14,31 @@ interface InGamePauseProps {
 	sendMessage: (gameObjectName: string, methodName: string, parameter?: ReactUnityEventParameter) => void;
 }
 
+enum ConfirmationAction {
+	None = "none",
+	Restart = "restart",
+	Exit = "exit"
+}
+
+
+
 const InGamePause: FC<InGamePauseProps> = ({
 	addEventListener,
 	removeEventListener,
 	sendMessage
 }) => {
 
-	const { inGamePauseActive, setInGamePauseActive } = useApplicationStore();
+	const { inGamePauseActive, setInGamePauseActive, gameModeActive } = useApplicationStore();
+	const [sfx, setSfx] = useState(true);
+	const [music, setMusic] = useState(true);
+	const [confirmationActive, setConfirmationActive] = useState(false);
+	const [confirmationAction, setConfirmationAction] = useState<ConfirmationAction>(ConfirmationAction.None);
 
 
 	const handleSetPauseMenu = useCallback((sfxMute: any, musicMute: any) => {
 		setInGamePauseActive(true);
-		console.log(sfxMute, musicMute);
-
+		sfxMute === 0 ? setSfx(true) : setSfx(false);
+		musicMute === 0 ? setMusic(true) : setMusic(false);
 	}, []);
 
 	useEffect(() => {
@@ -44,64 +58,118 @@ const InGamePause: FC<InGamePauseProps> = ({
 	}, [addEventListener, removeEventListener, handleUnpauseMenu]);
 
 
-	function handleCloseMenu() {
+	function handleClosePauseMenu() {
 		setInGamePauseActive(false);
 		sendMessage("UICanvas", "TogglePauseGame")
 	}
 
-	function handleRestartLevel() {
-		setInGamePauseActive(false);
-		sendMessage("UICanvas", "RestartLevel")
+	function handleMuteSFX() {
+		setSfx(!sfx);
+		sendMessage("UICanvas", "ToggleSFXFromReact");
+	}
+
+	function handleMuteMusic() {
+		setMusic(!music);
+		sendMessage("UICanvas", "ToggleMusicFromReact");
 	}
 
 
-	function handleExit() {
+
+
+	// CONFIRMATION
+	// Restart or Exit confirmation handling
+	function handleGameAction(action: ConfirmationAction, isConfirmation: boolean = false) {
+		if (!isConfirmation) {
+			setConfirmationActive(true);
+			setConfirmationAction(action);
+			return;
+		}
+
+		if (action === ConfirmationAction.None) {
+			setConfirmationActive(false);
+			setConfirmationAction(ConfirmationAction.None);
+			return;
+		}
+
+		const actions = {
+			[ConfirmationAction.Restart]: () => sendMessage("UICanvas", "RestartLevel"),
+			[ConfirmationAction.Exit]: () => sendMessage("UICanvas", "ExitToMainMenu")
+		};
+
 		setInGamePauseActive(false);
-		sendMessage("UICanvas", "ExitToMainMenu");
+		actions[action]?.();
+
+		setTimeout(() => {
+			setConfirmationActive(false);
+			setConfirmationAction(ConfirmationAction.None);
+		}, 1000);
 	}
-
-	// function handleResumeGame() {
-	// 	setInGamePauseActive(false);
-	// 	sendMessage("UICanvas", "TogglePauseGame")
-	// }
-
-
-
-	// useEffect(() => {
-	// 	if (inGamePauseActive === true) {
-	// 		const handleKeyPress = (event: KeyboardEvent) => {
-	// 			if (event.key.toLowerCase() === 'p') {
-	// 				closePauseMenu();
-	// 			}
-	// 		};
-	// 		window.addEventListener('keydown', handleKeyPress);
-	// 		return () => window.removeEventListener('keydown', handleKeyPress);
-	// 	}
-
-	// }, [inGamePauseActive]);
 
 
 	return (
 		<Dialog
 			open={inGamePauseActive}
 			onOpenChange={() => { }}
-			className="overflow-hidden max-w-xs h-64"
+			className="overflow-hidden max-w-xs"
 		>
-			<div className="relative z-10">
+			<div className="relative z-10 flex flex-col items-center justify-center">
 
-				<h2 className="mb-6 text-3xl font-orbitron text-center tracking-wide">Paused</h2>
-
-				<div className="space-y-3 outline-none">
-					<Button onClick={handleCloseMenu} variant={"secondary"} className="w-full shadow-none" tabIndex={-1}>Resume</Button>
-					<Button onClick={handleRestartLevel} variant={"secondary"} className="w-full" tabIndex={-1}>Restart</Button>
-					<Button onClick={handleExit} variant={"secondary"} className="w-full" tabIndex={-1}>Exit to Main Menu</Button>
+				<div className="mb-6 text-center">
+					{!confirmationActive
+						? <h2 className="text-3xl font-orbitron tracking-wide">Paused</h2>
+						: <h2 className="text-2xl font-orbitron tracking-wide">Are you sure?</h2>
+					}
+					<span className="text-sm text-muted-foreground">
+						{!confirmationActive
+							? <>{gameModeActive === "normal" ? "Normal Mode" : gameModeActive === "bossFight" ? "Boss Fight" : "AI Generated Level"}</>
+							: <>{confirmationAction === "restart" ? "Restart Game" : "Exit to main menu"}</>
+						}
+					</span>
 				</div>
 
+				{!confirmationActive &&
+					<>
+						<div className="space-y-3 outline-none">
+							<Button onClick={handleClosePauseMenu} variant={"secondary"} className="w-full shadow-none" tabIndex={-1}>Resume</Button>
+							<Button onClick={() => handleGameAction(ConfirmationAction.Restart)} variant={"secondary"} className="w-full" tabIndex={-1}>Restart</Button>
+							<Button onClick={() => handleGameAction(ConfirmationAction.Exit)} variant={"secondary"} className="w-full" tabIndex={-1}>Exit to Main Menu</Button>
+						</div>
 
 
-				{/* test
+						<div className="my-6 w-10 h-[1px] mx-auto bg-white/10"></div>
 
-				<button onClick={closePauseMenu}>Close</button> */}
+						<div className="relative flex items-center border rounded-2xl px-2">
+							<div className="flex items-center">
+								<Switch id="sfx" checked={sfx} onCheckedChange={handleMuteSFX} tabIndex={-1} />
+								<Label htmlFor="sfx" className="cursor-pointer pl-2 py-2">SFX</Label>
+							</div>
+
+							<div className="mx-6 w-[1px] h-4 bg-white/10" />
+
+							<div className="flex items-center">
+								<Switch id="music" checked={music} onCheckedChange={handleMuteMusic} tabIndex={-1} />
+								<Label htmlFor="music" className="cursor-pointer pl-2 py-2">Music</Label>
+							</div>
+						</div>
+					</>
+				}
+
+				{confirmationActive &&
+					<div className="w-full flex items-center space-x-2">
+						<Button onClick={() => handleGameAction(ConfirmationAction.None, true)} variant={"outline"} className="!flex-1 shadow-none" tabIndex={-1}>No</Button>
+						<Button
+							onClick={() => {
+								if (confirmationAction === ConfirmationAction.Restart) handleGameAction(ConfirmationAction.Restart, true);
+								else if (confirmationAction === ConfirmationAction.Exit) handleGameAction(ConfirmationAction.Exit, true);
+							}}
+							variant={"orange"}
+							className="!flex-1 shadow-none"
+							tabIndex={-1}
+						>
+							Yes
+						</Button>
+					</div>
+				}
 
 			</div>
 		</Dialog>
