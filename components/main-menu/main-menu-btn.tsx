170dbc35f7414f32aac10df801b229fc385a83f0
Shahen Algoo <shahenalgoo@gmail.com>
2025-01-01 13:34:46 +0400

    main menu

--- components/main-menu/main-menu-btn.tsx
+++ components/main-menu/main-menu-btn.tsx
@@ -22,12 +22,12 @@ const MainMenuButton: FC<MainMenuButtonProps> = ({ onClick, title, description }
 			<button
 				onClick={onClick}
 				onMouseDown={() => play()}
-				className="group w-[22rem] space-y-1 py-4 px-6 rounded-2xl text-left transition-all border border-white/10 hover:border-primary bg-white/10 hover:bg-primary/5"
+				className="group w-[22rem] py-3 px-4 rounded-2xl text-left transition-all border border-white/10 hover:border-primary bg-white/10 hover:bg-primary/5"
 			>
-				<div className={`flex items-center justify-between text-2xl tracking-normal transition-all group-hover:text-primary group-hover:tracking-wider`}>
+				<div className={`flex items-center justify-between text-xl tracking-normal transition-all group-hover:text-primary group-hover:tracking-wider`}>
 					{title} <MoveRight strokeWidth={2} className="mr-10 opacity-0 transition-all group-hover:mr-0 group-hover:opacity-100" />
 				</div>
-				<div className="transition-all text-white/70 group-hover:text-white">{description}</div>
+				<div className="transition-all text-sm text-white/70 group-hover:text-white">{description}</div>
 			</button>
 
 		</>
