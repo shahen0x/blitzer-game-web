9afffa8b3d4f5323cf14e3472fc0d797fb067b8e
Shahen Algoo <shahenalgoo@gmail.com>
2025-01-02 16:18:11 +0400

    s3 hostname fix

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ const nextConfig = {
 	images: {
 		remotePatterns: [{
 			protocol: 'https',
-			hostname: [
-				'amplify-awsamplifygen2-us-blitzergamedrivebucketad-ie4o1kx6ltqu.s3.us-west-2.amazonaws.com',
-				'amplify-d2lg0lyt69spz7-ma-blitzergamedrivebucketad-6dpkp9uaa3cq.s3.us-west-2.amazonaws.com'
-			]
-		}]
+			hostname: 'amplify-awsamplifygen2-us-blitzergamedrivebucketad-ie4o1kx6ltqu.s3.us-west-2.amazonaws.com'
+		},
+		{
+			protocol: 'https',
+			hostname: 'amplify-d2lg0lyt69spz7-ma-blitzergamedrivebucketad-6dpkp9uaa3cq.s3.us-west-2.amazonaws.com'
+		}
+		]
 	}
 }
 
